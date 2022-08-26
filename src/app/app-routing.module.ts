6774b57b0f2bb7abad1c363db27ed8684b44c990
Nelcione Dias <nelcionedias@gmail.com>
2022-08-26 11:19:50 -0300

    Route configuration, pagesModule, lazy Loading, routes child routing.modules components: Home and Details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //vincula as rotas do componente
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule) //lazy Loading
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
